27028e6fb1276aac18cb2d342e3df0c35cac0314
rzalka <jakub@air-jakub.home>
2024-04-16 15:38:02 +0200

    Dodanie zmian do dockerfile

--- docker/runserver.sh
+++ docker/runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it django-server python manage.py runserver
+docker exec -it django-server python manage.py runserver 0.0.0.0:8000
--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('account/', include('account.urls')),
+    path('account/', include('account.urls')),
     # path('food/', include('food.urls')),
     # path('recipies/', include('recipies.urls')),
     # path('user_rating/', include('user_rating.urls')),
