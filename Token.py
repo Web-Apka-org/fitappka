c802018e0e946817c8439b96cef2cf88bd36d88f
Wojtek <wojtek.deren@spoko.pl>
2024-06-06 14:54:33 +0200

    Implemented JWT based authentication, still needs implementing logging.

--- Token.py
+++ Token.py
@@ -5,27 +5,75 @@ from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 import secrets
 
 from django.conf import settings
+from rest_framework.permissions import BasePermission
 
 from account.models import User
 
 
-def generate(user_id: int = None) -> str, str:
+class WrongTokenError(Exception):
+    pass
+
+
+class UserDoesNotExist(Exception):
+    pass
+
+
+class JWTPermission(BasePermission):
+    '''
+    JWT based authentication.
+    '''
+    def has_permission(self, request, view) -> True | False:
+        token = str(request.META['HTTP_TOKEN'])
+
+        try:
+            decoded_token = jwt.api_jwt.decode_complete(
+                token,
+                settings.PUBLIC_KEY,
+                algorithms=['EdDSA']
+            )
+
+            header = decoded_token['header']
+
+            if 'user_id' not in header:
+                # logging
+                return False
+
+            if 'for' not in header:
+                # logging
+                return False
+
+            if header['for'] != 'access':
+                # logging
+                return False
+        except InvalidTokenError as ex:
+            # logging
+            return False
+        except ExpiredSignatureError as ex:
+            # logging
+            return False
+
+        return True
+
+
+def generate(user_id: int = None) -> (str, str):
     '''
     Generate two tokens, first for authenticated access and second
     for generate new tokens (refresh).
+
+    Throws TypeError.
     '''
     if user_id is None:
         raise TypeError('user_id is None')
 
     if not isinstance(user_id, int):
-        raise TypeError('user_id must be int.')
+        raise TypeError('\'user_id\' must be int.')
 
     today = datetime.now()
     access_expire = int((
             today + settings.JWT_TOKEN_EXPIRE).timestamp()
     )
     refresh_expire = int((
-            today + setting.JWT_REFRESH_TOKEN_EXPIRE).timestamp()
+            today + settings.JWT_REFRESH_TOKEN_EXPIRE).timestamp()
     )
 
     # access token
@@ -54,30 +102,24 @@ def generate(user_id: int = None) -> str, str:
     return access, refresh
 
 
-def get_user(token: str = None) -> Optional[User, None]:
+def get_user(token: str = None) -> User:
+    '''
+    Return User model instance from token.
+
+    Throws TypeError and UserDoesNotExist exceptions.
+    '''
     if token is None:
         raise TypeError('token is None')
 
     if not isinstance(token, str):
         raise TypeError('token must be str.')
 
-    header: dict
+    header = jwt.get_unverified_header(token)
+    user: User
 
     try:
-        decoded_token = jwt.api_jwt.decode_complete(
-            token,
-            settings.PUBLIC_KEY,
-            algorithms=['EdDSA']
-        )
+        user = User.objects.get(pk=header['user_id'])
+    except User.ObjectDoesNotExist:
+        raise UserDoesNotExist('User does not exist.')
 
-        header = decoded_token['header']
-    except ExpiredSignatureError as ex:
-        pass
-    except InvalidTokenError as ex:
-        pass
-
-    if 'user_id' not in header:
-        # logging here in future
-        return None
-
-    
+    return user
