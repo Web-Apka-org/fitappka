17c8098a7a92733cbd70ae9cf2ebc56ac35cd96b
Programeldev <wojtek.deren@spoko.pl>
2024-07-22 20:03:23 +0200

    Implemented adding or update user rate for Recipies.

--- user_rating/validators.py
+++ user_rating/validators.py
@@ -1,18 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator
 
-from .models import UserRating
-
-
-# def is_recipie_rated(recipie_id):
-#     if UserRating.objects.filter(recipie=recipie_id):
-#         raise ValidationError('User already rated this Recipie.')
 
 def rating_validator(value):
     rates = ['1', '2', '3', '4', '5', '6']
 
     for rate in rates:
-        if rate == value:
+        if value == rate:
             return
 
     raise ValidationError('Wrong rating value, only from 1 to 6.')
--- user_rating/views.py
+++ user_rating/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from extra import Token
+from extra.permissions import JWTPermission
+from extra.exceptions import WrongTokenError, UserDoesNotExist
+from extra.utils import ErrorResponse
+
+from .serializers import UserRatingSerializer
+from .models import UserRating
+
+
+class UserRatingView(APIView):
+    permission_classes = [JWTPermission]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRatingSerializer(data=request.POST)
+        serializer.is_valid(raise_exceptions=True)
+        token = request.META['HTTP_TOKEN']
+
+        try:
+            user = Token.get_user(token)
+        except (
+            WrongTokenError,
+            UserDoesNotExist
+        ) as ex:
+            return ErrorResponse(ex)
+        else:
+            data = serializer.data
+            data.update({'user': user})
+
+            UserRating.objects.update_or_create(user=user, defaults=data)
+
+            return Response(status=201)
